b1c9cff32d19e9d76f5832591f6c085c7b8fd605
Amakoye <charlesamakoye@gmail.com>
2024-11-24 15:13:35 +0300

    implemented doubly linked list node class

--- linked-lists/implementations/python/linked_list_nodes.py
+++ linked-lists/implementations/python/linked_list_nodes.py
@@ -30,3 +30,32 @@ class Node(Generic[T]):
         self.val = val
         self.next = next
 
+#  ----------------------------------------------------------------------
+class DoublyLinkedNode(Generic[T]):
+    """
+    Represents a node in a doubly linked list.
+
+    Each node contains a value and references to both the next and previous nodes,
+    allowing for bidirectional traversal of the list.
+    
+    Type Parameters:
+        T: The type of value stored in the node.
+
+    """
+ 
+    def __init__(self, val:T, prev:Optional['DoublyLinkedNode[T]']=None, next:Optional['DoublyLinkedNode[T]']=None) -> None:
+        """
+        Initializes a new node with the given value and optional references to adjacent nodes.
+        
+        Args:
+            val (T): The value to be stored in the node.
+
+            prev (Optional[DoublyLinkedNode[T]]): Reference to the previous node. 
+                Defaults to None, indicating this node is the first in the list.
+                
+            next (Optional[DoublyLinkedNode[T]]): Reference to the next node.
+                Defaults to None, indicating this node is the last in the list.
+        """
+        self.val = val
+        self.prev = prev
+        self.next = next
